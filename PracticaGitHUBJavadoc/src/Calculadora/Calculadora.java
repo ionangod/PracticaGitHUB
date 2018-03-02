c2d9877f46d527e226803836bddd298bae1d685f
ionan <ionan@MintEntornos>
2018-03-02 22:05:40 +0100

    Javadoc creado
    Hemos creado un javadoc para el proyecto

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -1,41 +1,73 @@
 package Calculadora;
 
 public class Calculadora {
-
 	double op1;
 	double op2;
 	
-	
+	/**
+	 * Aqui asignamos los numero que van a operar
+	 * @param op1: operando 1
+	 * @param op2: operando 2
+	 */
 	public Calculadora(double op1, double op2) {
 		this.op1 = op1;
 		this.op2 = op2;
+		/**
+		 * Aqui vamos a construirlo
+		 */
 	}
 	
 	public double suma() {
+
 		return op1+op2;
+		/**
+		 * Aqui vamos a sumarlo
+		 * @return op+op2: devuelve la suma
+		 */
 	}
 	
 	public double resta() {
 		return op1-op2;
+		/**
+		 * Aqui vamos a restarlo
+		 * @return op-op2: devuelve la resta
+		 */
 	}
 	
 	public double division() {
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
+			/**
+			 * @throw Da error si se divide por 0
+			 */
 		}
 		return op1/op2;
+		/**
+		 * Aqui vamos a dividirlo
+		 * @return op/op2: devuelve la division
+		 */
 	}
 	public double potencia() {
 		if (op1==0)
 			return 1;
+		/**
+		 * @return devuelve 1 si el op2 es 0
+		 */
 		if (op2<0)
 			return -Math.pow(op1, op2);
 		return Math.pow(op1, op2);
+		/**
+		 * Aqui vamos a elevarlo
+		 * @return Math.pow(op1,op2): devuelve la potencia
+		 */
 	}
 
 	@Override
 	public String toString() {
 		return "Calculadora de Ionan:\nOperando1 = " + op1 + "\nOperando2 =" + op2;
+	/**
+	 * @return devulete el autor y los numero que operan
+	 */
 	}
 	
 	//TODO: A�adir la operaci�n potencia, que devuelve
